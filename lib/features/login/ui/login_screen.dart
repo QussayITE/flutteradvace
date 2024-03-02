f6454d7c5daa3b613b6728115e04b66fc7586681
Qussay Nomer <q.nomer@vicworldwide.com>
2024-03-02 20:36:06 +0400

    login logic
    - added di
    - added app regex
    - integrated with login api
    - added login repo & cubit
    - added login state with validation

--- lib/features/login/ui/login_screen.dart
+++ lib/features/login/ui/login_screen.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutteradvace/core/helpers/spacing.dart';
 import 'package:flutteradvace/core/theming/styles.dart';
 import 'package:flutteradvace/core/wedigets/app_text_button.dart';
-import 'package:flutteradvace/core/wedigets/app_text_form_field.dart';
+import 'package:flutteradvace/features/login/data/models/login_request_body.dart';
+import 'package:flutteradvace/features/login/logic/login_cubit.dart';
 import 'package:flutteradvace/features/login/ui/wedigts/already_have_account_text.dart';
+import 'package:flutteradvace/features/login/ui/wedigts/email_and_password.dart';
+import 'package:flutteradvace/features/login/ui/wedigts/login_bloc_listener.dart';
 import 'package:flutteradvace/features/login/ui/wedigts/terms_and_coditions_text.dart';
 
-class LoginScreen extends StatefulWidget {
+class LoginScreen extends StatelessWidget {
   const LoginScreen({Key? key}) : super(key: key);
-
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
-  final formKey = GlobalKey<FormState>();
-  bool isObscureText = true;
 
   @override
   Widget build(BuildContext context) {
@@ -38,51 +34,44 @@ class _LoginScreenState extends State<LoginScreen> {
                 style: TextStyles.font14GrayRegular,
               ),
               verticalSpace(36),
-              Form(
-                key: formKey,
-                child: Column(
-                  children: [
-                    const AppTextFormField(hintText: "Email"),
-                    verticalSpace(18),
-                    AppTextFormField(
-                      hintText: "Password",
-                      isObscureText: isObscureText,
-                      suffixIcon: GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            isObscureText = !isObscureText;
-                          });
-                        },
-                        child: Icon(isObscureText
-                            ? Icons.visibility_off
-                            : Icons.visibility),
-                      ),
+              Column(
+                children: [
+                  const EmailAndPassword(),
+                  verticalSpace(24),
+                  Align(
+                    alignment: AlignmentDirectional.centerEnd,
+                    child: Text(
+                      'Forget Password?',
+                      style: TextStyles.font13BlueRegular,
                     ),
-                    verticalSpace(24),
-                    Align(
-                      alignment: AlignmentDirectional.centerEnd,
-                      child: Text(
-                        'Forget Password?',
-                        style: TextStyles.font13BlueRegular,
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
               verticalSpace(40),
               AppTextButton(
                 buttonText: 'Login',
                 textStyle: TextStyles.font16WhiteSemiBold,
-                onPressed: () {},
+                onPressed: () {
+                  validateThenLogin(context);
+                },
               ),
               verticalSpace(16),
               const TermsAndConditionsText(),
               verticalSpace(60),
               const AlreadyHaveAccountText(),
+              const LoginBlocListener(),
             ],
           ),
         ),
       ),
     );
+  }
+
+  void validateThenLogin(BuildContext context) {
+    if (context.read<LoginCubit>().formKey.currentState!.validate()) {
+      context.read<LoginCubit>().emitLoginStates(LoginRequestBody(
+          email: context.read<LoginCubit>().emailController.text,
+          password: context.read<LoginCubit>().passwordController.text));
+    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutteradvace/core/di/dependency_injection.dart';
 import 'package:flutteradvace/core/routing/app_router.dart';
 import 'package:flutteradvace/doc_app.dart';
 
 void main() {
+  setupGetIt();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
