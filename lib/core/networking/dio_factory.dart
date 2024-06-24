f6b7556dbbff801921a7925254adca2ebfb798a6
Qussay Nomer <q.nomer@vicworldwide.com>
2024-06-24 06:08:51 +0400

    Save token

--- lib/core/networking/dio_factory.dart
+++ lib/core/networking/dio_factory.dart
@@ -1,4 +1,6 @@
 import 'package:dio/dio.dart';
+import 'package:flutteradvace/core/helpers/constants.dart';
+import 'package:flutteradvace/core/helpers/shared_pref_helper.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 class DioFactory {
@@ -23,6 +25,12 @@ class DioFactory {
     }
   }
 
+  static void setTokenIntoHeaderAfterLogin(String token) {
+    dio?.options.headers = {
+      'Authorization': 'Bearer $token',
+    };
+  }
+
   static void addDioInterceptor() {
     dio?.interceptors.add(
       PrettyDioLogger(
@@ -33,11 +41,11 @@ class DioFactory {
     );
   }
 
-  static void addDioHeader() {
+  static void addDioHeader() async {
     dio?.options.headers = {
       "Accept": "application/json",
       "Authorization":
-          "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczovL3ZjYXJlLmludGVncmF0aW9uMjUuY29tL2FwaS9hdXRoL2xvZ2luIiwiaWF0IjoxNzE4NzE5MzM0LCJleHAiOjE3MTg4MDU3MzQsIm5iZiI6MTcxODcxOTMzNCwianRpIjoiY09DWExnekVSY0FsbnRUbCIsInN1YiI6IjEyMTYiLCJwcnYiOiIyM2JkNWM4OTQ5ZjYwMGFkYjM5ZTcwMWM0MDA4NzJkYjdhNTk3NmY3In0.9-EO0XvJhXYn9IuCR8K5OjweXtnxuXU2AkaUz4w2iMM"
+          "Bearer ${await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken)}"
     };
   }
 }
--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,14 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutteradvace/core/di/dependency_injection.dart';
+import 'package:flutteradvace/core/helpers/constants.dart';
+import 'package:flutteradvace/core/helpers/extensions.dart';
+import 'package:flutteradvace/core/helpers/shared_pref_helper.dart';
 import 'package:flutteradvace/core/routing/app_router.dart';
 import 'package:flutteradvace/doc_app.dart';
 
-Future<void> main() async {
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   setupGetIt();
   // To fix texts being hidden bug in flutter_screenutil in release mode.
   await ScreenUtil.ensureScreenSize();
+  await checkIfLoggedInUser();
   runApp(DocApp(
     appRouter: AppRouter(),
   ));
+}
+
+checkIfLoggedInUser() async {
+  String? userToken =
+  await SharedPrefHelper.getSecuredString(SharedPrefKeys.userToken);
+  if (!userToken.isNullOrEmpty()) {
+    isLoggedInUser = true;
+  } else {
+    isLoggedInUser = false;
+  }
 }
